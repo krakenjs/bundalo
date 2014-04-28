3cad3560431e0c5be93928fe8ec4f7d64735a95f
Matt Edelman <medelman@paypal.com>
2014-04-28 11:56:07 -0700

    adding basic functionality

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+var fs = require('fs'),
+	spud = require('spud'),
+	resolver = require('file-resolver'),
+	i18n,
+	locality,
+	type;
+
 exports.dust = function() {
 	return {
 		"create": function(config) {
@@ -9,7 +16,20 @@ exports.dust = function() {
 				    "fallback": "en-US"
 				}
 			*/
-
+			i18n = config.i18n;
+			locality = config.locality;
+			type = config.type;
+		},
+		"get": function(config, callback) {
+			//sample config {"bundle": "errors/server", "key": "error", "model": {"name": "Will Robinson"}}
+			var reso = resolver.create({ root: i18n.contentPath, ext: type, fallback: i18n.fallback});
+			var props = reso.resolve("errors/server", locality).file || i18n.contentPath;
+			var readStream = fs.createReadStream(props);
+			spud.deserialize(readStream, 'properties', function (err, data) {
+				//console.log(err || data);
+				//console.log('Conversion complete.');
+				callback(data);
+			});
 		}
 	}
 }
