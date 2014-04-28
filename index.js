56a199e48f17ba664c74663668866cb68df8c491
Matt Edelman <medelman@paypal.com>
2014-04-28 11:27:22 -0700

    adding console.log

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
-module.exports.dust = function() {
+exports.dust = function() {
 	return {
 		"create": function(config) {
-			//sample config: {"i18n": {below}, "locality": locality, "type": "properties|json"}
+			console.log("bundalo['dust'].create config", config);
+			//sample config: {"i18n": {below}, "locality": "en-US", "type": "properties|json"}
 			/*	
 				"i18n": {
 				    "contentPath": "path:./locales",
 				    "fallback": "en-US"
 				}
 			*/
+
 		}
 	}
 }
