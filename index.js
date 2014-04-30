ea46b9cf5cfadde0ce1254836f00864473612324
Matt Edelman <medelman@paypal.com>
2014-04-30 15:57:59 -0700

    adding async

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 var fs = require('fs'),
 	spud = require('spud'),
 	resolver = require('file-resolver'),
-	pollutedust = require('dustjs-linkedin'),
 	freshy = require('freshy'),
 	dust = freshy.freshy('dustjs-linkedin'),
+	async = require('async'),
 	i18n,
 	locality,
 	type;
 
+var dustRender = function(cacheKey, model, cb) {
+	dust.render(cacheKey, model || {}, function (err, out) {
+		spud.deserialize(new Buffer(out, 'utf8'), 'properties', function (err, data) {
+			cb(null, data);
+		});
+	});
+};
 exports.dust = (function () {
 	return {
 		"create": function (config) {
@@ -15,8 +22,8 @@ exports.dust = (function () {
 			//sample config: {"i18n": {below}, "locality": "en-US", "type": "properties|json"}
 			/*	
 			 "i18n": {
-			 "contentPath": "path:./locales",
-			 "fallback": "en-US"
+				 "contentPath": "path:./locales",
+				 "fallback": "en-US"
 			 }
 			 */
 			i18n = config.i18n;
@@ -25,37 +32,27 @@ exports.dust = (function () {
 
 		},
 		"get": function (config, callback) {
-			//sample config {"bundle": "errors/server", "model": {"name": "Will Robinson"}}
+			//single bundle config {"bundle": "errors/server", "model": {"name": "Will Robinson"}}
+			//multiple bundle config {"bundle": ["errors/server", "errors/client"], "model": {"name": "Will Robinson"}}
 			//console.log("bundalo.get:config", config);
 
-			if (dust.cache && dust.cache[config.bundle]) {
-				dust.render(config.bundle, config.model || {}, function (err, out) {
-					spud.deserialize(new Buffer(out, 'utf8'), 'properties', function (err, data) {
-						callback(null, data);
-					});
-				});
-				return;
-			}
+			//create a bundle key based on bundle name and locality info
 			var reso = resolver.create({ root: i18n.contentPath, ext: type, fallback: i18n.fallback});
 			var bundleFile = reso.resolve(config.bundle, locality).file || i18n.contentPath;
-			//console.log("bundleFile", bundleFile);
+			var cacheKey = bundleFile.split(i18n.contentPath)[1];
 
+			if (dust.cache && dust.cache[cacheKey]) {
+				dustRender(cacheKey, config.model, callback);
+				return;
+			}
 
+			//not yet in cache
 			fs.readFile(bundleFile, {}, function handleBundleBuffer(err, bundleBuffer) {
-				var compiled = dust.compile(bundleBuffer.toString(), config.bundle);
+				var compiled = dust.compile(bundleBuffer.toString(), cacheKey);
 				dust.loadSource(compiled);
-				//console.log("compiled", compiled);
-
-
-				dust.render(config.bundle, config.model || {}, function (err, out) {
-					spud.deserialize(new Buffer(out, 'utf8'), 'properties', function (err, data) {
-						callback(null, data);
-					});
-				});
+				dustRender(cacheKey, config.model, callback);
 			});
 
 		}
 	}
 })();
-
-exports.js = exports.dust;
