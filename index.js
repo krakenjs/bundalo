81410d950c8ed2e3c31a16cbd2539fbb2cf84cb5
Matt Edelman <medelman@paypal.com>
2014-04-30 18:07:28 -0700

    adding none implementation

--- index.js
+++ index.js
@@ -6,7 +6,10 @@ var fs = require('fs'),
 	async = require('async'),
 	i18n,
 	locality,
-	type;
+	type,
+	none = {};
+
+none.cache = {};
 
 var dustRender = function(cacheKey, model, cb) {
 	dust.render(cacheKey, model || {}, function (err, out) {
@@ -15,11 +18,12 @@ var dustRender = function(cacheKey, model, cb) {
 		});
 	});
 };
+
 exports.dust = (function () {
 	return {
 		"create": function (config) {
 			//console.log("bundalo['dust'].create config", config);
-			//sample config: {"i18n": {below}, "locality": "en-US", "type": "properties|json"}
+			//sample config: {"i18n": {below}, "locality": "en-US"}
 			/*	
 			 "i18n": {
 				 "contentPath": "path:./locales",
@@ -28,7 +32,6 @@ exports.dust = (function () {
 			 */
 			i18n = config.i18n;
 			locality = config.locality;
-			type = config.type;
 
 		},
 		"get": function (config, callback) {
@@ -43,7 +46,7 @@ exports.dust = (function () {
 			configBundle.forEach(function(bundle) {
 				bundleRenderer[bundle] = function(cb) {
 					//create a bundle key based on bundle name and locality info
-					var reso = resolver.create({ root: i18n.contentPath, ext: type, fallback: i18n.fallback});
+					var reso = resolver.create({ root: i18n.contentPath, ext: 'properties', fallback: i18n.fallback});
 					var bundleFile = reso.resolve(bundle, locality).file || i18n.contentPath;
 					var cacheKey = bundleFile.split(i18n.contentPath)[1];
 
@@ -65,7 +68,69 @@ exports.dust = (function () {
 
 			async.parallel(bundleRenderer, function(err, results) {
 				var returnVal = results;
-				console.log("async results", results);
+				//console.log("async results", results);
+				//if only one bundle object, remove top level key
+				if (Object.keys(results).length === 1) {
+					returnVal = results[Object.keys(results)[0]];
+				}
+				callback(null, returnVal);
+			});
+
+
+		}
+	}
+})();
+
+exports.none = (function () {
+	return {
+		"create": function (config) {
+			//console.log("bundalo['none'].create config", config);
+			//sample config: {"i18n": {below}, "locality": "en-US"}
+			/*
+			 "i18n": {
+			 "contentPath": "path:./locales",
+			 "fallback": "en-US"
+			 }
+			 */
+			i18n = config.i18n;
+			locality = config.locality;
+		},
+		"get": function (config, callback) {
+			//single bundle config {"bundle": "errors/server"}
+			//multiple bundle config {"bundle": ["errors/server", "errors/client"]}
+
+			var bundleReader = {};
+
+			//assume config.bundle is either an array of bundle strings or a string
+			var configBundle = (config.bundle.constructor === Array) ? config.bundle : [config.bundle];
+
+			configBundle.forEach(function(bundle) {
+				bundleReader[bundle] = function(cb) {
+					//create a bundle key based on bundle name and locality info
+					var reso = resolver.create({ root: i18n.contentPath, ext: 'properties', fallback: i18n.fallback});
+					var bundleFile = reso.resolve(bundle, locality).file || i18n.contentPath;
+					var cacheKey = bundleFile.split(i18n.contentPath)[1];
+
+					if (none.cache && none.cache[cacheKey]) {
+						//console.log("bundalo:incache:",cacheKey);
+						cb(null, none.cache[cacheKey]);
+						return;
+					}
+
+					//not yet in cache
+					fs.readFile(bundleFile, {}, function handleBundleBuffer(err, bundleBuffer) {
+						//console.log("bundalo:outcache:",cacheKey);
+						spud.deserialize(bundleBuffer, 'properties', function (err, bundleJSON) {
+							none.cache[cacheKey] = bundleJSON;
+							cb(null, none.cache[cacheKey]);
+						});
+					});
+				};
+			});
+
+			async.parallel(bundleReader, function(err, results) {
+				var returnVal = results;
+				//console.log("async results", results);
 				//if only one bundle object, remove top level key
 				if (Object.keys(results).length === 1) {
 					returnVal = results[Object.keys(results)[0]];
