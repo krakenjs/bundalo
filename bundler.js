4972d0bd5fc047dd08b0e77d60d713322998e148
Aria Stewart <aredridel@dinhe.net>
2015-07-22 12:49:29 -0400

    Factor out loopalo and use monkeymap instead

--- bundler.js
+++ bundler.js
@@ -20,43 +20,73 @@
 'use strict';
 var fs = require('fs');
 var spud = require('spud');
-var loopalo = require('./lib/loopalo');
 var iferr = require('iferr');
 var async = require('async');
+
+var monkeymap = require('monkeymap');
 
 var fileResolver = require('file-resolver');
 
 
 function Bundler(config) {
 	this.resolver = fileResolver.create({ root: config.contentPath, ext: 'properties', fallback: config.fallback});
-	this.doCache = (config.cache !== undefined && config.cache === false) ? false : true;
-	this.cache = {};
+	this.cache = (config.cache !== undefined && config.cache === false) ? null : {};
 }
 
 Bundler.prototype.get = function (config, callback) {
-	var that = this;
+    var cache = this.cache;
+    var resolver = this.resolver;
+    var bundle = Array.isArray(config.bundle) ? makeObj(config.bundle) : config.bundle;
+    monkeymap(bundle, function (file, next) {
+        async.compose(readCached(cache), resolve(resolver, config.locale || config.locality))(file, next);
+    }, callback);
+};
 
-	function noneBundler(bundleFile, cb) {
-		if (that.cache && that.cache[bundleFile]) {
-			async.ensureAsync(cb(null, that.cache[bundleFile]));
+function resolve(resolver, locale) {
+    return function (filename, callback) {
+        try {
+            var file = resolver.resolve(filename, locale).file;
+            if (!file) {
+				var err = new Error("ENOENT: no such file or directory '" + filename + "'");
+				err.code = 'ENOENT';
+				err.path = filename;
+				throw err;
+            }
+            callback(null, file);
+        } catch (e) {
+            callback(e);
+        }
+    };
+}
+
+function readCached(cache) {
+
+	return function noneBundler(bundleFile, cb) {
+		if (cache && cache[bundleFile]) {
+			async.ensureAsync(cb(null, cache[bundleFile]));
 			return;
 		}
 		//not yet in cache
-		fs.readFile(bundleFile, iferr(callback, function handleBundleBuffer(bundleBuffer) {
+		fs.readFile(bundleFile, iferr(cb, function handleBundleBuffer(bundleBuffer) {
 			try {
 				var parsed = spud.parse(bundleBuffer.toString());
-				if (that.doCache) {
-					that.cache[bundleFile] = parsed;
+				if (cache) {
+					cache[bundleFile] = parsed;
 				}
 				safe(cb)(null, parsed);
 			} catch (e) {
 				safe(cb)(e);
 			}
 		}));
-	}
+	};
+}
 
-	loopalo(config.bundle, config, this.resolver, noneBundler, callback);
-};
+function makeObj(arr) {
+    return arr.reduce(function (a, e) {
+        a[e] = e;
+        return a;
+    }, {});
+}
 
 function safe(cb) {
     return function() {
@@ -71,7 +101,7 @@ function safe(cb) {
 }
 
 Bundler.prototype.__cache = function () {
-	return this.cache;
+	return this.cache || {};
 };
 
 module.exports = Bundler;
